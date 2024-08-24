b5679af6b35d4b0fe7506843482ec7374e8d4ad2
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-08-23 21:13:08 -0400

    Request

--- src/metadata.csv.py
+++ src/metadata.csv.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import sys
+from urllib.request import Request, urlopen
 
-df_pop = pd.read_excel("http://ville.montreal.qc.ca/pls/portal/url/ITEM/55637C4923B8B03EE0530A930132B03E", sheet_name="01_Population, Densité", skiprows=2).iloc[:-4, :]
+URL = "http://ville.montreal.qc.ca/pls/portal/url/ITEM/55637C4923B8B03EE0530A930132B03E"
+
+req = Request(
+    url=URL, 
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+
+df_pop = pd.read_excel(URL, sheet_name="01_Population, Densité", skiprows=2).iloc[:-4, :]
 df_pop.rename(columns={'Unnamed: 0': 'arrondissement', 'Population en 2016': '2016', 'Population en 2011': '2011'}, inplace=True)
 df_pop = df_pop[~df_pop.arrondissement.isin(['Autres villes', 'Ville de Montréal','AGGLOMÉRATION DE MONTRÉAL'])]
 # df_pop['arrondissement'] = df_pop['arrondissement'].apply(normalize_name)
